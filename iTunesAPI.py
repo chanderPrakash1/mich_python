031f38fb40bffb8971547e6b80f0af31dcf03f7f
CHANDERPRAKASH <chiku.s.cp@hotmail.com>
2019-03-29 19:05:06 +0530

    Created ITunesAPI.py
    Learning Using Requets to get data from API

---
+++ iTunesAPI.py
@@ -0,0 +1,9 @@
+import requests
+import json
+parameters={'terms':'jack johnson'}
+base_url='https://itunes.apple.com/search'
+iTunes_response=requests.get(base_url,params=parameters)
+print(iTunes_response.url)
+print(iTunes_response.status_code)
+iTunes_data=json.loads(iTunes_response.text)
+print(iTunes_data)
